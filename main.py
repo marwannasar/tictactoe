bfe9b9ebed1585a8c500bb77d8b0e6c4dc4e33d0
Mar2 <marwan.nasar@gmail.com>
2020-09-22 07:33:36 +0000

    formatting changes and added comments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#tictactoe created with python, run the program to play, it uses number inputs to select your position. Have fun!
+
 import copy 
 
 current_player = 'X'
 next_player = 'O'
-selection = 0
+selection = 9
 winner = ''
 
 gameboard = [
@@ -11,8 +13,9 @@ gameboard = [
   ['-','-','-']
 ]
 
+#displays the board and the inputs that correspond to each square
 def showBoard():
-  print("Board:         Inputs: ")
+  print("Board:            Inputs: ")
   for i in range (3):
     for j in range(3):
       print(gameboard[i][j],"  ", end = '')
@@ -24,7 +27,8 @@ def showBoard():
         else:
           print("      6 | 7 | 8")
         print ('\n')
-  
+
+#checks if the selection in yourTurn is valid
 def checkValid(selection):
   valid = False
   if selection<=2 and selection>=0:
@@ -39,9 +43,9 @@ def checkValid(selection):
 
   return valid
 
-
+#takes the user input, uses checkValid to see if it hasn't been played before
 def yourTurn(player):
-  print("your turn to play, ", current_player)
+  print("your turn to play,", current_player)
   selection = int(input("Choose a position from 0 to 8: "))
   while selection > 8 or selection <0:
     selection = int(input("That's an invalid input, please choose a position from 0 to 8: "))
@@ -54,6 +58,7 @@ def yourTurn(player):
     print('\n')
     return selection
 
+#inserts the users position choice into the gameboard
 def insertSelection(selection):
   if selection<=2 and selection>=0:
     gameboard[0][selection] = current_player
@@ -62,6 +67,7 @@ def insertSelection(selection):
   elif selection<=8 and selection>=6:
     gameboard[2][selection-6] = current_player
 
+#checks if the game ended in a tie, the game can't be over due to a win becuase we check that first
 def checkTie():
   tie = True
   for i in range (3):
@@ -71,7 +77,7 @@ def checkTie():
         return tie
   return tie
 
-
+#checks if someone won vertically
 def checkRows():
   row1same = gameboard[0][0] == gameboard[0][1] == gameboard[0][2] != "-"
   row2same = gameboard[1][0] == gameboard[1][1] == gameboard[1][2] != "-"
@@ -92,7 +98,7 @@ def checkRows():
 
   return False
 
-
+#checks if someone won horizontally
 def checkColumns():
   col1same = gameboard[0][0] == gameboard[1][0] == gameboard[2][0] != "-"
   col2same = gameboard[0][1] == gameboard[1][1] == gameboard[2][1] != "-"
@@ -113,7 +119,7 @@ def checkColumns():
 
   return False
 
-
+#checks if someone won diagonally
 def checkDiags():
   diag1same = gameboard[0][0] == gameboard[1][1] == gameboard[2][2] != "-"
   diag2same = gameboard[0][2] == gameboard[1][1] == gameboard[2][0] != "-"
@@ -130,7 +136,7 @@ def checkDiags():
 
   return False
 
-
+#checks all 3 win cases: vertical, diagonal, horizontal
 def checkGameOver():
   checkRows()
   checkColumns()
@@ -143,7 +149,7 @@ def checkGameOver():
 
 
 
-
+# the actual game which keeps looping, breaks out when the game ends in a win or a tie
 while True:
   showBoard()
   selection = yourTurn(current_player)
@@ -160,6 +166,8 @@ while True:
     showBoard()
     print("The game has ended in a tie, thanks for playing :)")
     break
+
+  #next person's turn so we flip X and O
   temp = copy.deepcopy(current_player)
   current_player = copy.deepcopy(next_player)
   next_player = copy.deepcopy(temp)
